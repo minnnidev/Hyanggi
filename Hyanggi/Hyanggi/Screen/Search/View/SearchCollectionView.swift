6231592481132ca0c5a17b214d38468b770057b8
Mini <minnnidev@gmail.com>
2024-04-04 01:41:28 +0900

    [Feat] SearchViewController layout

---
+++ Hyanggi/Hyanggi/Screen/Search/View/SearchCollectionView.swift
@@ -0,0 +1,75 @@
+//
+//  SearchCollectionView.swift
+//  Hyanggi
+//
+//  Created by 김민 on 4/4/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SearchCollectionView: UICollectionViewCell {
+
+    static let identifier = "SearchCollectionView"
+
+    private let titleLabel = UILabel()
+    private let sentenceLabel = UILabel()
+    private let contentLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setViews()
+        setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setViews() {
+        contentView.backgroundColor = .white
+        
+        titleLabel.do {
+            $0.text = "딥디크 오로즈"
+        }
+
+        sentenceLabel.do {
+            $0.text = "꽃맛이 날 것 같은 솜사탕 냄새"
+        }
+
+        contentLabel.do {
+            $0.text = "akdjsfljdfkajdls;kfadfadjfkajwleijldkjalifjdlajfkesdakdjsflddajkfshakjsdhflkajhsdlkfjhalsdkjfrads"
+            $0.numberOfLines = 0
+        }
+
+        [titleLabel, sentenceLabel, contentLabel].forEach {
+            $0.font = .systemFont(ofSize: 14, weight: .regular)
+        }
+    }
+
+    private func setConstraints() {
+        [titleLabel, sentenceLabel, contentLabel].forEach {
+            contentView.addSubview($0)
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(12)
+            $0.centerX.equalToSuperview()
+        }
+
+        sentenceLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.leading.equalTo(titleLabel)
+            $0.centerX.equalToSuperview()
+        }
+
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(sentenceLabel.snp.bottom)
+            $0.leading.equalTo(titleLabel)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-12)
+        }
+    }
+}
---
+++ Hyanggi/Hyanggi/Screen/Search/View/SearchViewController.swift
@@ -0,0 +1,91 @@
+//
+//  SearchViewController.swift
+//  Hyanggi
+//
+//  Created by 김민 on 3/21/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SearchViewController: BaseViewController {
+
+    private let searchCollectionView = UICollectionView(frame: .zero,
+                                                            collectionViewLayout: UICollectionViewLayout()).then {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+
+        $0.collectionViewLayout = flowLayout
+        $0.backgroundColor = .clear
+        $0.showsVerticalScrollIndicator = false
+    }
+
+    private let searchBar = UISearchBar()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setCollectionView()
+    }
+
+    override func setNavigationBar() {
+        super.setNavigationBar()
+
+        navigationItem.title = "검색"
+    }
+
+    override func setConstraints() {
+        [searchBar, searchCollectionView].forEach {
+            view.addSubview($0)
+        }
+
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(40)
+        }
+
+        searchCollectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+
+    func setCollectionView() {
+        searchCollectionView.dataSource = self
+        searchCollectionView.delegate = self
+
+        searchCollectionView.register(SearchCollectionView.self, forCellWithReuseIdentifier: SearchCollectionView.identifier)
+    }
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionView.identifier, for: indexPath) as? SearchCollectionView else { return UICollectionViewCell() }
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 90)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()
+        detailVC.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
